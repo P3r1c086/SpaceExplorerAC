6d71e49fed48437fed54cedb1a8179a0c6292262
Pedro Aguilar Fernández <pajaros33@gmail.com>
2024-10-15 20:44:29 +0200

    Changed Merge to Combine to Optimize Code

--- data/src/main/java/es/architectcoders/data/repository/FavoriteRepository.kt
+++ data/src/main/java/es/architectcoders/data/repository/FavoriteRepository.kt
@@ -6,6 +6,7 @@ import es.architectcoders.domain.Apod
 import es.architectcoders.domain.NasaItem
 import es.architectcoders.domain.Photo
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.merge
 import javax.inject.Inject
@@ -15,11 +16,22 @@ class FavoriteRepository @Inject constructor(
     private val ApodLocalDataSource: ApodLocalDataSource
 ){
 
-    fun getFavoriteList(): Flow<Flow<List<NasaItem>>> {
+//    fun getFavoriteList(): Flow<Flow<List<NasaItem>>> {
+//        val allFavoritesApods: Flow<List<Apod>> = ApodLocalDataSource.getFavoriteApods
+//        val allFavoritesRovers: Flow<List<Photo>> = RoversLocalDataSource.getFavoritePhoto
+//
+//        return merge(flowOf(allFavoritesApods, allFavoritesRovers))
+//    }
+
+    fun getFavoriteList(): Flow<List<NasaItem>> {
         val allFavoritesApods: Flow<List<Apod>> = ApodLocalDataSource.getFavoriteApods
         val allFavoritesRovers: Flow<List<Photo>> = RoversLocalDataSource.getFavoritePhoto
 
-        return merge(flowOf(allFavoritesApods, allFavoritesRovers))
+        return combine(allFavoritesApods, allFavoritesRovers) { apods, rovers ->
+            val combinedFavorites = mutableListOf<NasaItem>()
+            combinedFavorites.addAll(apods)
+            combinedFavorites.addAll(rovers)
+            combinedFavorites
+        }
     }
-
 }
