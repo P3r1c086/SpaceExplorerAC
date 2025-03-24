41558e75258ee90122dac6b4a1445c0b5527d5ee
Pedro Aguilar Fernández <pajaros33@gmail.com>
2025-03-24 11:30:11 +0100

    expand button ui test implemented

--- app/src/androidTest/java/es/architectcoders/spaceexplorer/ui/main/RoversInstrumentationTest.kt
+++ app/src/androidTest/java/es/architectcoders/spaceexplorer/ui/main/RoversInstrumentationTest.kt
@@ -1,18 +1,29 @@
 package es.architectcoders.spaceexplorer.ui.main
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.rule.GrantPermissionRule
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import es.architectcoders.data.datasource.RoversRemoteDataSource
+import es.architectcoders.spaceexplorer.R
 import es.architectcoders.spaceexplorer.apptestshared.defaultFakePhotos
 import es.architectcoders.spaceexplorer.apptestshared.defaultFakePhotos2
 import es.architectcoders.spaceexplorer.data.server.MockWebServerRule
-import es.architectcoders.spaceexplorer.data.server.fromJson
 import es.architectcoders.spaceexplorer.framework.database.roverDb.RoversDao
 import es.architectcoders.spaceexplorer.framework.database.roverDb.fromDomain
+import es.architectcoders.spaceexplorer.ui.MainActivity
+import es.architectcoders.spaceexplorer.ui.rovers.RoversAdapter
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.runTest
-import okhttp3.mockwebserver.MockResponse
+import kotlinx.coroutines.test.setMain
+import org.hamcrest.CoreMatchers
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
@@ -31,8 +42,11 @@ class RoversInstrumentationTest {
     @get:Rule(order = 1)
     val mockWebServerRule = MockWebServerRule()
 
+    @get:Rule(order = 2)
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
     //Esta rule funciona al correr la app en un dispositivo con API 29 o inferior
-    @get:Rule(order = 2)//segundo que se configure el permiso
+    @get:Rule(order = 3)
     val storagePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
         "android.permission.WRITE_EXTERNAL_STORAGE"
     )
@@ -45,7 +59,6 @@ class RoversInstrumentationTest {
 
     @Before
     fun setUp(){
-
         hiltRule.inject()
     }
 
@@ -90,5 +103,47 @@ class RoversInstrumentationTest {
         photos.fold({ throw Exception(it.toString()) }) {
             assertEquals("1204630", it[0].id)
         }
+    }
+
+    @Test
+    fun when_click_in_expand_button_the_text_is_showed() {
+        // Click en el fragmento de rovers
+        Espresso.onView(ViewMatchers.withId(R.id.roversFragment))
+            .perform(ViewActions.click())
+
+        // Esperar para que los datos carguen (usando una acción personalizada)
+        Espresso.onView(ViewMatchers.isRoot()).perform(waitFor(5000))
+
+        // Verificar que RecyclerView está visible
+        Espresso.onView(ViewMatchers.withId(R.id.rvRovers))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        // Click en el primer elemento del RecyclerView
+        Espresso.onView(ViewMatchers.withId(R.id.rvRovers))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RoversAdapter.ViewHolder>(0, ViewActions.click())
+            )
+
+        // Click en el botón de expandir dentro del primer elemento del RecyclerView
+        Espresso.onView(ViewMatchers.withId(R.id.rvRovers))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RoversAdapter.ViewHolder>(
+                    0,
+                    clickChildViewWithId(R.id.ibExpand)
+                )
+            )
+
+        // Hacer scroll hasta el primer elemento para asegurarse de que es visible
+        Espresso.onView(ViewMatchers.withId(R.id.rvRovers))
+            .perform(RecyclerViewActions.scrollToPosition<RoversAdapter.ViewHolder>(0))
+
+        // Verificar que el contenido expandido (llData) es visible solo dentro del primer elemento
+        Espresso.onView(
+            CoreMatchers.allOf(
+                ViewMatchers.withId(R.id.llData),
+                ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE),
+                ViewMatchers.isDescendantOfA(RecyclerViewMatchers.atPosition(0, R.id.rvRovers))
+            )
+        ).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 }
